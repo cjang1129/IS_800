6a71d4e515c2563e770d357f65f2fbf67810e5c9
cjang1129 <60392869+cjang1129@users.noreply.github.com>
2025-05-15 12:17:14 -0400

    Update EC2_Metadata.py

--- FinalProject/EC2_Metadata.py
+++ FinalProject/EC2_Metadata.py
@@ -2,6 +2,12 @@ import boto3
 
 client = boto3.client('ec2')
 response = client.describe_instances()
+
+/* prints metadata */
+print(response)
+
+/* shows instance ids */
+
 for instance in response['Reservations']:
     for instance in instance['Instances']:
         print(instance['InstanceId'])
